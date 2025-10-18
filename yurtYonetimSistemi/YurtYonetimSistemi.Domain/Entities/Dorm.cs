e7c8bfdb66e9fbf977171e72419542ed55c98dfc
Suleyman Meral <mrlslymn02@gmail.com>
2025-10-18 09:42:18 +0300

    feat(domain): create DormConfiguration (#1)

--- yurtYonetimSistemi/YurtYonetimSistemi.Domain/Entities/Dorm.cs
+++ yurtYonetimSistemi/YurtYonetimSistemi.Domain/Entities/Dorm.cs
@@ -5,7 +5,6 @@ namespace YurtYonetimSistemi.Domain.Entities;
 public sealed class Dorm:BaseEntity<int>
 {
     public string Name { get; set; } = null!;
-    public string Description { get; set; } = null!;
     public string Address { get; set; } = null!;
     public string PhoneNumber { get; set; } = null!;
     public string Email { get; set; } = null!;
