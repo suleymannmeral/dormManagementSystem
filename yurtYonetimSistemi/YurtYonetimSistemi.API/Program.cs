2ba7bda0fc9799062215e74be3f7d1e2eef940e6
Suleyman Meral <mrlslymn02@gmail.com>
2025-10-18 23:08:50 +0300

    feat(application): create IGenericRepository #5

--- yurtYonetimSistemi/YurtYonetimSistemi.API/Program.cs
+++ yurtYonetimSistemi/YurtYonetimSistemi.API/Program.cs
@@ -1,7 +1,5 @@
-using Microsoft.EntityFrameworkCore;
-using YurtYonetimSistemi.Persistence;
-using YurtYonetimSistemi.Persistence.Context;
-using YurtYonetimSistemi.Persistence.Options;
+
+using YurtYonetimSistemi.Persistence.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -11,20 +9,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 
 // DbContext
-builder.Services.AddDbContext<AppDbContext>(options =>
-{
-
-    var connectionStrings =
-        builder.Configuration.GetSection(ConnectionStringOption.Key).Get<ConnectionStringOption>();
-
-    options.UseSqlServer(connectionStrings!.SqlServer, sqlServerOptionsAction =>
-    {
-        sqlServerOptionsAction.MigrationsAssembly(typeof(PersistenceAssembly).Assembly.FullName);
-    });
-
-});
-
-
+builder.Services.AddPersistenceExt(builder.Configuration);
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
---
+++ yurtYonetimSistemi/YurtYonetimSistemi.Application/Contracts/Persistence/IGenericRepository.cs
@@ -0,0 +1,19 @@
+﻿
+
+using System.Linq.Expressions;
+
+namespace YurtYonetimSistemi.Application.Contracts.Persistence;
+
+public interface IGenericRepository<T, TId> where T : class where TId : struct
+{
+    Task<bool> AnyAsync(TId id);
+    Task<bool> AnyAsync(Expression<Func<T, bool>> predicate);
+    Task<List<T>> GetAllAsync(); //veritabanından verileri filtreleyip döndürür
+    Task<List<T>> GetAllPagedAsync(int pageNumber, int pageSize); //veritabanından verileri filtreleyip döndürür
+
+    IQueryable<T> Where(Expression<Func<T, bool>> predicate);
+    ValueTask<T?> GetByIdAsync(int id);
+    ValueTask AddAsync(T entity);
+    void Update(T entity);
+    void Delete(T entity);
+}
