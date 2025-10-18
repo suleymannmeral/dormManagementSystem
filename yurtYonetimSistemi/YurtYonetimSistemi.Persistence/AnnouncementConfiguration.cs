0115b90e8e35fb3a1fd8e389331421942195132e
Suleyman Meral <mrlslymn02@gmail.com>
2025-10-18 09:44:30 +0300

    feat(domain): create AnnouncementConfiguration (#1)

---
+++ yurtYonetimSistemi/YurtYonetimSistemi.Persistence/AnnouncementConfiguration.cs
@@ -0,0 +1,18 @@
+﻿
+
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using YurtYonetimSistemi.Domain.Entities;
+
+namespace YurtYonetimSistemi.Persistence;
+
+public class AnnouncementConfiguration : IEntityTypeConfiguration<Announcement>
+{
+    public void Configure(EntityTypeBuilder<Announcement> builder)
+    {
+        builder.HasKey(a => a.Id);
+        builder.Property(a => a.Title).IsRequired().HasMaxLength(50);
+        builder.Property(a => a.Description).IsRequired().HasMaxLength(500);
+
+    }
+}
