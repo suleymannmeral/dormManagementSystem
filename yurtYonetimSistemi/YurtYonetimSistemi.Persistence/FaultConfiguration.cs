39fefd020fc7c32b5bb8d28a6156396a47668d83
Suleyman Meral <mrlslymn02@gmail.com>
2025-10-18 09:52:35 +0300

    feat(domain): create FaultConfiguration (#1)

---
+++ yurtYonetimSistemi/YurtYonetimSistemi.Persistence/FaultConfiguration.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using YurtYonetimSistemi.Domain.Entities;
+
+namespace YurtYonetimSistemi.Persistence;
+
+public class FaultConfiguration : IEntityTypeConfiguration<Fault>
+{
+    public void Configure(EntityTypeBuilder<Fault> builder)
+    {
+       builder.HasKey(f => f.Id);
+       builder.Property(f => f.Title).IsRequired().HasMaxLength(60);
+       builder.Property(f => f.Description).IsRequired().HasMaxLength(500);
+
+        builder.HasOne(f => f.Student)
+            .WithMany()          
+            .HasForeignKey(f => f.StudentId)
+            .IsRequired() 
+            .OnDelete(DeleteBehavior.Restrict); // İlişkili öğrenci silindiğinde Fault kaydının silinmesini engeller
+
+        builder.HasOne(f => f.Room) 
+            .WithMany() 
+            .HasForeignKey(f => f.RoomId)
+            .IsRequired()
+            .OnDelete(DeleteBehavior.Restrict); 
+    }
+}
