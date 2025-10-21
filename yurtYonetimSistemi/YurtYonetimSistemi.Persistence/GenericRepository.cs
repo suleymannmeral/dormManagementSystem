7f4c9bf2d2851e3057f0db11500e4eeb501c7cb3
Suleyman Meral <mrlslymn02@gmail.com>
2025-10-21 14:32:45 +0300

    feat(persistence): GenericRepo methods

--- yurtYonetimSistemi/YurtYonetimSistemi.Persistence/GenericRepository.cs
+++ yurtYonetimSistemi/YurtYonetimSistemi.Persistence/GenericRepository.cs
@@ -1,5 +1,5 @@
 ﻿
-
+using Microsoft.EntityFrameworkCore;
 using System.Linq.Expressions;
 using YurtYonetimSistemi.Application.Contracts.Persistence;
 using YurtYonetimSistemi.Domain.Entities.Common;
@@ -9,48 +9,32 @@ namespace YurtYonetimSistemi.Persistence;
 
 public class GenericRepository<T, TId>(AppDbContext context) : IGenericRepository<T, TId> where T : BaseEntity<TId> where TId : struct
 {
-    public ValueTask AddAsync(T entity)
-    {
-        throw new NotImplementedException();
-    }
+    protected AppDbContext Context = context;
 
-    public Task<bool> AnyAsync(TId id)
-    {
-        throw new NotImplementedException();
-    }
+
+    private readonly DbSet<T> _dbSet = context.Set<T>();
+
+    public async ValueTask AddAsync(T entity)=>await _dbSet.AddAsync(entity);
+
+    public Task<bool> AnyAsync(TId id)=> _dbSet.AnyAsync(x => x.Id.Equals(id));
 
     public Task<bool> AnyAsync(Expression<Func<T, bool>> predicate)
     {
-        throw new NotImplementedException();
+        return _dbSet.AnyAsync(predicate);
     }
+    public void Delete(T entity)=>_dbSet.Remove(entity);
 
-    public void Delete(T entity)
-    {
-        throw new NotImplementedException();
-    }
-
-    public Task<List<T>> GetAllAsync()
-    {
-        throw new NotImplementedException();
-    }
+    public Task<List<T>> GetAllAsync()=>_dbSet.ToListAsync();
 
     public Task<List<T>> GetAllPagedAsync(int pageNumber, int pageSize)
     {
-        throw new NotImplementedException();
+        return _dbSet.Skip((pageNumber - 1) * pageSize).Take(pageSize).ToListAsync();
     }
 
-    public ValueTask<T?> GetByIdAsync(int id)
-    {
-        throw new NotImplementedException();
-    }
+    public ValueTask<T?> GetByIdAsync(int id)=>_dbSet.FindAsync(id);
 
-    public void Update(T entity)
-    {
-        throw new NotImplementedException();
-    }
+    public void Update(T entity)=>_dbSet.Update(entity);
 
-    public IQueryable<T> Where(Expression<Func<T, bool>> predicate)
-    {
-        throw new NotImplementedException();
-    }
+    public IQueryable<T> Where(Expression<Func<T, bool>> predicate)=> _dbSet.Where(predicate).AsQueryable().AsNoTracking();
+
 }
