551fd8bef5c3ef7fcb5b25ac02bf59c445d89beb
Suleyman Meral <mrlslymn02@gmail.com>
2025-10-23 14:25:15 +0300

    DI Register for UnitOfWork class #9

--- yurtYonetimSistemi/YurtYonetimSistemi.Persistence/Extensions/DependencyInjection.cs
+++ yurtYonetimSistemi/YurtYonetimSistemi.Persistence/Extensions/DependencyInjection.cs
@@ -2,6 +2,7 @@
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using YurtYonetimSistemi.Application.Contracts.Persistence;
+using YurtYonetimSistemi.Persistence;
 using YurtYonetimSistemi.Persistence.Context;
 using YurtYonetimSistemi.Persistence.Faults;
 using YurtYonetimSistemi.Persistence.Meals;
@@ -10,25 +11,32 @@ using YurtYonetimSistemi.Persistence.Options;
 using YurtYonetimSistemi.Persistence.Rooms;
 using YurtYonetimSistemi.Persistence.Students;
 
-namespace YurtYonetimSistemi.Persistence.Extensions;
-
 public static class DependencyInjection
 {
     public static IServiceCollection AddPersistenceExt(this IServiceCollection services, IConfiguration configuration)
     {
         services.AddDbContext<AppDbContext>(options =>
         {
-            var connectionStrings =
-                configuration.GetSection(ConnectionStringOption.Key).Get<ConnectionStringOption>();
+            var connectionStrings = configuration
+                .GetSection(ConnectionStringOption.Key)
+                .Get<ConnectionStringOption>();
 
-            options.UseSqlServer(connectionStrings!.SqlServer, sqlServerOptionsAction =>
+            options.UseSqlServer(connectionStrings!.SqlServer, sqlOptions =>
             {
-    
-                sqlServerOptionsAction.MigrationsAssembly(typeof(PersistenceAssembly).Assembly.FullName);
+                sqlOptions.MigrationsAssembly(typeof(PersistenceAssembly).Assembly.FullName);
             });
-
         });
-        // use scrutor to register all repositories in assembly (later)
+
+        services.AddRepositories();
+
+        services.AddScoped(typeof(IGenericRepository<,>), typeof(GenericRepository<,>));
+        services.AddScoped<IUnitOfWork, UnitOfWork>();
+
+        return services;
+    }
+
+    private static void AddRepositories(this IServiceCollection services)
+    {
         services.AddScoped<IAnnouncementRepository, AnnouncementRepository>();
         services.AddScoped<IRoomRepository, RoomRepository>();
         services.AddScoped<IDormRepository, DormRepository>();
@@ -37,10 +45,5 @@ public static class DependencyInjection
         services.AddScoped<IMenuRepository, MenuRepository>();
         services.AddScoped<IStaffRepository, StaffRepository>();
         services.AddScoped<IStudentRepository, StudentRepository>();
-        services.AddScoped(typeof(IGenericRepository<,>), typeof(GenericRepository<,>));
-
-
-        return services;
     }
-
 }
---
+++ yurtYonetimSistemi/YurtYonetimSistemi.Persistence/UnitOfWork.cs
@@ -0,0 +1,10 @@
+﻿using YurtYonetimSistemi.Application.Contracts.Persistence;
+using YurtYonetimSistemi.Persistence.Context;
+
+namespace YurtYonetimSistemi.Persistence;
+
+public class UnitOfWork(AppDbContext context) : IUnitOfWork
+{
+    public Task<int> SaveChangesAsync() => context.SaveChangesAsync();
+
+}
