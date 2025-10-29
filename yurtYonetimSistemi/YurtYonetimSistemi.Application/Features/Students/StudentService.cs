2ca445a26a4a3e8982b1d9debdaa0e468e7eba89
Suleyman Meral <mrlslymn02@gmail.com>
2025-10-29 15:02:36 +0300

    add getByIdMethod to StudentService

--- yurtYonetimSistemi/YurtYonetimSistemi.Application/Features/Students/StudentService.cs
+++ yurtYonetimSistemi/YurtYonetimSistemi.Application/Features/Students/StudentService.cs
@@ -1,8 +1,30 @@
-﻿using YurtYonetimSistemi.Application.Contracts.Persistence;
+﻿using System.Net;
+using YurtYonetimSistemi.Application.Contracts.Identity;
+using YurtYonetimSistemi.Application.Contracts.Persistence;
 
 namespace YurtYonetimSistemi.Application.Features.Students;
 
 public class StudentService(IStudentRepository studentRepository,
-    IUnitOfWork unitOfWork):IStudentService
+    IUnitOfWork unitOfWork,
+    IUserService userService):IStudentService
 {
+    public async Task<ServiceResult<StudentDto>> GetStudentByIdAsync(int id)
+    {
+        var student = await studentRepository.GetByIdAsync(id);
+        // Check if staff exists
+        if (student is null)
+        {
+            return ServiceResult<StudentDto>.Fail("Student not found", HttpStatusCode.NotFound);
+        }
+        var fullName = await userService.GetFullNameByUserIdAsync(student.UserId);
+        var studentDto = new StudentDto(
+            student.Id,
+            student.UserId,
+            fullName!,
+            student.RoomId,
+            student.Department,
+            student.University
+        );
+        return ServiceResult<StudentDto>.Success(studentDto);
+    }
 }
